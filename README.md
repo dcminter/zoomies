967ce5023633ab610d278846247f8d9dc0343e0e
Dave Minter <dave@paperstack.com>
2021-07-18 21:53:38 +0200

    Use v4l directly instead of invoking the command line client.

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ This can be done from the command line (during use of the camera by another app)
 v4l2-ctl -d /dev/video2 --set-ctrl=zoom_absolute=150
 ```
 
-The GUI tool will allow the appropriate zoom level to be selected with a slider (device, range. etc. to be hardcoded at least initially).
+The GUI tool will allow the appropriate zoom level to be selected with a scrollbar (range. etc. to be hardcoded at least initially).
+
+Clang must be installed (on Ubuntu, `apt install clang`) so that the Rust V4L library dependency can compile. Failure to do so will result in the following error message:
+```
+error: failed to run custom build command for `v4l-sys v0.2.0`
+```
 
 ## Notes
 
@@ -16,10 +21,11 @@ The GUI tool will allow the appropriate zoom level to be selected with a slider 
 
 ## TODO
 
+  * Read current zoom level so we can initialise the slider to that!
   * ~~See if there's a simpler way to pass around the state to make it available in the closure.~~ I *think* passing the Cell around is a sane way to have the value be both mutable and available to the closure.
   * ~~Add visual feedback of the zoom level~~
   * ~~Fix wonkiness on the slider control range~~ Much less wonky now! Using `connect_local` seems to be the trick! 
-  * Better handling of the device(s) and ranges for the camera
-  * Better error handling for the v4l command (e.g. what if it's not installed!)
+  * ~~Better handling of the ~~device(s) and ranges for the camera~~ (Uses v4l directly now)
+  * ~~Better error handling for the v4l command (e.g. what if it's not installed!)~~ N/A
   * ~~Display the current zoom level~~ (Added)
-  * Use v4l2 API more directly so there's no need to invoke the v4l2-ctl binary (see [v4l2-ctl implementation](https://github.com/gjasny/v4l-utils/tree/master/utils/v4l2-ctl) and e.g. [rust v4l crate](https://docs.rs/v4l/0.12.1/v4l/))
+  * ~~Use v4l2 API more directly so there's no need to invoke the v4l2-ctl binary (see [v4l2-ctl implementation](https://github.com/gjasny/v4l-utils/tree/master/utils/v4l2-ctl) and e.g. [rust v4l crate](https://docs.rs/v4l/0.12.1/v4l/))~~ In progress on this branch!
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::cell::Cell;
-use std::process::Command;
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::fs::{DirEntry, read_to_string};
+use std::io::{Error, ErrorKind};
+use std::path::{Path, PathBuf};
 
 use gtk::{Adjustment, Application, ApplicationWindow, Scrollbar};
 use gtk::prelude::*;
+use v4l::control::Description;
+use v4l::prelude::*;
 
-static VALUE_CHANGED_SIGNAL: &str = "value-changed";
-static ZOOM_MIN: f64 = 100.0;
-static ZOOM_MAX: f64 = 500.0;
+static GTK_VALUE_CHANGED_SIGNAL: &str = "value-changed";
+static V4L_SYS_DEVICE_PATH: &str = "/sys/class/video4linux";
+static BRIO_ZOOM_CONTROL_NAME: &str = "Zoom, Absolute";
 static ZOOM_CLICK: f64 = 10.0; // Minimum change before we re-issue the svl2-ctl command to change Zoom level
 
 fn main() {
+    let (zoom_min, zoom_max) = establish_range();
+
     let state = Cell::new(100.0);
 
     let application = Application::builder()
@@ -28,20 +36,20 @@ fn main() {
         let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 2);
 
         // Slider: Note that the adjustment values aren't honoured by the slider in the way I expected. Need to figure out what I'm doing wrong here.
-        let adjustment = Adjustment::new(ZOOM_MIN, ZOOM_MIN, ZOOM_MAX, ZOOM_CLICK, 0.0, 0.0);
+        let adjustment = Adjustment::new(zoom_min, zoom_min, zoom_max, ZOOM_CLICK, 0.0, 0.0);
         let scrollbar = Scrollbar::new(gtk::Orientation::Horizontal, Option::from(&adjustment));
         hbox.pack_start(&scrollbar, true, true, 0);
 
         // Display the current zoom level
-        let label = gtk::Label::new(Some((ZOOM_MIN as i64).to_string().as_str()));
+        let label = gtk::Label::new(Some((zoom_min as i64).to_string().as_str()));
         hbox.pack_start(&label, false, false, 2);
 
         let state_copy = state.clone();
-        let _ = scrollbar.connect_local(VALUE_CHANGED_SIGNAL, false, move |values| {
+        let _ = scrollbar.connect_local(GTK_VALUE_CHANGED_SIGNAL, false, move |values| {
             for value in values {
                 let scroll = value.get::<Scrollbar>().unwrap();
                 let scrolled_to = scroll.value();
-                adjusted(scrolled_to, &state_copy);
+                adjusted(zoom_min, zoom_max, scrolled_to, &state_copy);
                 label.set_text((state_copy.get() as i64).to_string().as_str());
             }
             None
@@ -54,28 +62,104 @@ fn main() {
     application.run();
 }
 
-fn adjusted(value: f64, state: &Cell<f64>) {
-    if moved_enough(value, state) {
+fn adjusted(zoom_min: f64, zoom_max: f64, value: f64, state: &Cell<f64>) {
+    if moved_enough(zoom_min, zoom_max, value, state) {
         state.replace(value);
-        zoom(state.get());
+        zoom(state.get() as i32);
     }
 }
 
-fn moved_enough(value: f64, state: &Cell<f64>) -> bool {
-    if value <= ZOOM_MIN || value >= ZOOM_MAX {
+fn moved_enough(zoom_min: f64, zoom_max: f64, value: f64, state: &Cell<f64>) -> bool {
+    if value <= zoom_min || value >= zoom_max {
         state.get() != value
     } else {
         (state.get() - value).abs() > ZOOM_CLICK
     }
 }
 
-fn zoom(value: f64) {
-    // Works-on-my-machine but should configure by reading current devices and available ranges
-    // and with better errors if the command's not available and so on.
-    Command::new("v4l2-ctl")
-        .arg("-d")
-        .arg("/dev/video2")
-        .arg(format!("--set-ctrl=zoom_absolute={}", value))
-        .output()
-        .expect("Well oops.");
+fn zoom(value: i32) {
+    let dev = brio_device().unwrap();
+    let controls = dev.query_controls().unwrap();
+    let identifiers: Vec<u32> = controls.into_iter()
+        .filter(|c| c.name == BRIO_ZOOM_CONTROL_NAME)
+        .map(|c| c.id)
+        .collect();
+
+    match identifiers[..] {
+        [id] => {
+            // Note - set_control doesn't currently accept any control type except Value!
+            let _ = dev.set_control(id, v4l::Control::Value(value));
+        }
+        _ => {
+            eprintln!("No identifier found for the zoom control on the device");
+        }
+    }
+}
+
+fn brio_device() -> Result<Device, Error> {
+    // Find the device. I suspect the "proper" way to do this is to look
+    // in /sys/class/video4linux/*/uevent where DEVNAME is specified, but
+    // I expect this will mostly be ok using the path name:
+    let paths = std::fs::read_dir(Path::new(V4L_SYS_DEVICE_PATH)).unwrap();
+    let brio_devices: HashMap<i32, OsString> = paths.into_iter()
+        .filter_map(|p| p.ok())
+        .filter(correct_device_name)
+        .map(to_device_entry)
+        .collect();
+
+    // Everything gathered should be a "Logitech BRIO" device, so we just want the one with index 0 (I'm
+    // not going to worry about the possibility of having TWO plugged in!)
+    match brio_devices.get(&0) {
+        Some(device) => {
+            let mut device_path = PathBuf::new();
+            device_path.push("/dev");
+            device_path.push(device);
+
+            Device::with_path(device_path)
+        }
+        None => {
+            Err(Error::from(ErrorKind::NotFound))
+        }
+    }
+}
+
+fn to_device_entry(value: DirEntry) -> (i32, OsString) {
+    let device: OsString = value.file_name();
+    let index: i32 = read_device_index(&value);
+    (index, device)
+}
+
+fn read_device_index(entry: &DirEntry) -> i32 {
+    let mut path = entry.path();
+    path.push("index");
+    let output = read_to_string(path).unwrap().trim().to_string();
+    output.parse::<i32>().unwrap()
+}
+
+fn correct_device_name(entry: &DirEntry) -> bool {
+    let mut path = entry.path();
+    path.push("name");
+    let output = read_to_string(path).unwrap().trim().to_string();
+    output == "Logitech BRIO"
+}
+
+fn establish_range() -> (f64, f64) {
+    let dev = brio_device().unwrap();
+    let controls = dev.query_controls().unwrap();
+
+    let zoom_controls: Vec<Description> = controls.into_iter()
+        .filter(|c| c.name == BRIO_ZOOM_CONTROL_NAME)
+        .collect();
+
+    match &zoom_controls[..] {
+        [control_description] => {
+            eprintln!("Control: {:?} has range {:?} to {:?}", control_description.name, control_description.minimum, control_description.maximum);
+            (control_description.minimum as f64, control_description.maximum as f64)
+        }
+        _ => {
+            // TODO: Return an error here instead
+            eprintln!("No zoom control found on the device");
+            (100.0, 500.0)
+        }
+    }
 }
